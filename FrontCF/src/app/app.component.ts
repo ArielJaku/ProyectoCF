7b3b54f7582c556d7707027d4e05109508c5c5b8
Ariel Jakubowski <ari_jaku@hotmail.com>
2025-11-05 21:30:18 -0300

    Agregando header, menu y home

--- FrontCF/src/app/app.component.ts
+++ FrontCF/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,MenuComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ FrontCF/src/app/menu/menu.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [CommonModule,RouterModule],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+  abierto = false;
+
+  abrirMenu() {
+    this.abierto = !this.abierto;
+  }
+}
